6ee60888be1220c71712893f4e9ebfd42e8d677c
xxxfussanxxx <mitareshi0424@gmail.com>
2024-06-20 03:13:16 +0900

    リポジトリをクローンしてきて、ビルドが行えるようにした。

--- app/src/main/java/org/goodppy/BuildChecker.java
+++ app/src/main/java/org/goodppy/BuildChecker.java
@@ -1,10 +1,37 @@
 package org.goodppy;
 
+import java.io.File;
+import java.io.IOException;
+
+
 /**
  * ビルドが正しく行えるかをチェックするクラス
  */
 public class BuildChecker {
 	public BuildChecker() {
 		// コンストラクタ
+	}
+
+	/**
+	 * @param localPath
+	 */
+	public void BuildCheck(String localPath) {
+		try {
+			System.out.println("Start the build...");
+			ProcessBuilder builder = new ProcessBuilder();
+			builder.command("gradle", "build");
+			builder.directory(new File(localPath));
+			Process process = builder.start();
+			Integer exitCode = process.waitFor();
+			if (exitCode != 0) {
+				System.out.println("Build failed");
+				return;
+			}
+			System.out.println("Build success");
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return;
 	}
 }
--- app/src/main/java/org/goodppy/RepositoryController.java
+++ app/src/main/java/org/goodppy/RepositoryController.java
@@ -1,10 +1,41 @@
 package org.goodppy;
 
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 /**
  * 評価するリポジトリに関する操作を行うクラス
  */
 public class RepositoryController {
-	public RepositoryController() {
-	
+	private String repositoryUrl;
+
+	private String localPath;
+
+	/**
+	 * @param repositoryUrl
+	 * @param localPath
+	 */
+	public RepositoryController(String repositoryUrl, String localPath) {
+		this.repositoryUrl = repositoryUrl;
+		this.localPath = localPath;
 	}
+
+	public void GitClone() {
+		try {
+			System.out.println("Repository is being cloned ...");
+			Git.cloneRepository()
+					.setURI(repositoryUrl)
+					.setDirectory(new File(localPath))
+					.call();
+			System.out.println("Repository has been cloned.");
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+			System.out.println("Repository has been failed to clone.");
+		}
+
+		return;
+	}
+
 }
